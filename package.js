d3863ac2df395041b07f5538a7ca3db1311336ca
Dave van Soest <dave.soest@gfk.com>
2014-09-09 16:16:25 +0200

    Ready for new package system (Meteor 0.9).

--- package.js
+++ package.js
@@ -1,28 +1,33 @@
 Package.describe({
-    summary: "Trail - Pluggable logging for Meteor."
+    summary: "Trail - Pluggable logging for Meteor.",
+    version: "0.1.2",
+    name: "mrt:trail",
+    git: "https://github.com/tbknl/meteor-trail.git"
 });
 
-Package.on_use(function(api) {
-    api.use(['livedata'], ['server']);
-    api.use(['deps'], ['client']);
+Package.onUse(function(api) {
+    api.versionsFrom('0.9.0');
+
+    api.use('livedata', 'server');
+    api.use('deps', 'client');
 
     // Allow us to detect 'insecure'.
     api.use('insecure', {weak: true});
 
-    api.add_files('trail.js', ['server', 'client']);
-    api.add_files('trail_settings.js', ['server', 'client']);
-    api.add_files('trail_instance.js', ['server', 'client']);
-    api.add_files('trail_transports.js', ['server', 'client']);
-    api.add_files('trail_share-server.js', ['server']);
-    api.add_files('trail_share-client.js', ['client']);
-    api.add_files('trail_transport_console.js', ['server', 'client']);
-    api.add_files('trail_transport_mongo.js', ['server']);
+    api.addFiles('trail.js', ['server', 'client']);
+    api.addFiles('trail_settings.js', ['server', 'client']);
+    api.addFiles('trail_instance.js', ['server', 'client']);
+    api.addFiles('trail_transports.js', ['server', 'client']);
+    api.addFiles('trail_share-server.js', ['server']);
+    api.addFiles('trail_share-client.js', ['client']);
+    api.addFiles('trail_transport_console.js', ['server', 'client']);
+    api.addFiles('trail_transport_mongo.js', ['server']);
 
     api.export && api.export('Trail', ['server', 'client']);
 });
 
-Package.on_test(function (api) {
-  api.use(['trail', 'tinytest', 'test-helpers']);
+Package.onTest(function (api) {
+  api.use(['tinytest', 'test-helpers']);
 
-  api.add_files('trail_tests.js', ['server']);
+  api.addFiles('trail_tests.js', ['server']);
 });
